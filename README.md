4239ba0e7ddfe0859238cb4a92d04f7e8a7e1f9e
chenlinbo <993443108@qq.com>
2023-09-06 09:35:36 +0800

    add pool

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 - [原地构建小根堆](./src/heap.py)
 - [KMP](./src/kmp.py)
 - [LRU](./src/lru.py)
+- [协程池](./src/pool.go)
 - [排序算法](./src/sorts.py)
---
+++ src/pool.go
@@ -0,0 +1,96 @@
+package src
+
+import (
+	"errors"
+	"log"
+	"sync"
+	"time"
+)
+
+var (
+	ErrInvalidPoolCapacity = errors.New("invalid Pool Capacity")
+	ErrPoolAlreadyClosed   = errors.New("pool Already Closed")
+)
+
+const (
+	RUNNING = 1
+	STOPPED = 0
+)
+
+type Task struct {
+	Handler func(v ...interface{})
+	Params  []interface{}
+}
+
+type Pool struct {
+	capacity uint64
+	workers  uint64
+	status   int64
+	tasks    chan *Task
+	sync.Mutex
+}
+
+func NewPool(capacity uint64) (*Pool, error) {
+	if capacity <= 0 {
+		return nil, ErrInvalidPoolCapacity
+	}
+	p := &Pool{
+		capacity: capacity,
+		status:   RUNNING,
+		tasks:    make(chan *Task, capacity),
+	}
+	// health check
+	go func() {
+		p.Lock()
+		defer p.Unlock()
+		if p.workers == 0 && len(p.tasks) > 0 {
+			p.workers++
+			p.run()
+		}
+	}()
+	return p, nil
+}
+
+func (p *Pool) Close() {
+	p.Lock()
+	p.status = STOPPED
+	p.Unlock()
+	for len(p.tasks) > 0 {
+		time.Sleep(1 * time.Millisecond)
+	}
+	close(p.tasks)
+}
+
+func (p *Pool) Put(task *Task) error {
+	p.Lock()
+	defer p.Unlock()
+	if p.status == STOPPED {
+		return ErrPoolAlreadyClosed
+	} else if p.workers < p.capacity {
+		p.workers++
+		p.run()
+	}
+	p.tasks <- task
+	return nil
+}
+
+func (p *Pool) run() {
+	go func() {
+		defer func() {
+			p.Lock()
+			p.workers--
+			p.Unlock()
+			if r := recover(); r != nil {
+				log.Printf("Worker panic: %s\n", r)
+			}
+		}()
+		for {
+			select {
+			case task, ok := <-p.tasks:
+				if ok {
+					task.Handler(task.Params...)
+				}
+			}
+		}
+	}()
+}
